c524bfa58ac8b47b1d94c0f8f052fbf2595788f3
vinceallen <vince@lot18.com>
2012-07-05 10:48:50 -0400

    started refactoring to a module format using exports, etc

--- js/app.js
+++ js/app.js
@@ -1,5 +1,181 @@
 /*global window, PVector, setInterval, document */
 
+(function (exports) {
+	
+	var World = (function () {
+	
+		function World () {
+			this.width = window.innerWidth;
+		  this.height = window.innerHeight;
+		  this.gravity = PVector.create(0, 0.01);
+		  this.wind = PVector.create(0.01, 0);
+		}
+	
+		World.prototype.devicemotion = function () {
+			var e = arguments[0];
+			if (window.orientation === 0) {
+				this.gravity = PVector.create(e.accelerationIncludingGravity.x * .01, e.accelerationIncludingGravity.y * -.01); // portrait
+			} else if (window.orientation === -90) {
+				this.gravity = PVector.create(e.accelerationIncludingGravity.y * .01, e.accelerationIncludingGravity.x * .01);
+			} else {
+				this.gravity = PVector.create(e.accelerationIncludingGravity.y * -.01, e.accelerationIncludingGravity.x * -.01);
+			}
+		};
+		
+		return World;
+	
+	}());
+	
+	exports.World = World;
+	
+}(window));
+
+(function (exports) {
+	
+	var ParticleSystem = (function () {
+		
+		var funcRef;
+		
+		function ParticleSystem (options) {
+			
+			var options = options || {};
+			
+			this.particles = [];
+		  this.idCount = 0;
+			this.world = options.World;
+		}
+	
+		ParticleSystem.prototype.stepSystem = function (particles, world) {
+			return function () {
+				var i;
+
+		    for (i = particles.length - 1; i >= 0; i -= 1) {
+		      if (!particles[i].dead) {
+		        particles[i].step(world);
+		        particles[i].draw();
+		      } else {
+		        particles[i].destroy(particles);
+		      }
+		    }
+			};
+		};
+		
+		ParticleSystem.prototype.start = function () {
+			funcRef = this.stepSystem(this.particles, this.world);
+			this.interval = setInterval(funcRef, 16);
+		};
+		
+		return ParticleSystem;
+	
+	}());
+	
+	exports.ParticleSystem = ParticleSystem;
+	
+}(window));
+
+(function (exports) {
+	
+	var Particle = (function () {
+	
+		function Particle (options) {
+			
+			var options = options || {},
+				myDiv;
+			
+			this.acceleration = PVector.create(0, 0);
+	    this.velocity = PVector.create(0, 0);
+	    this.location = PVector.create(options.World.width/2, options.World.height/2);
+	    this.scale = 1;
+	    this.opacity = 1;
+	    this.life = 0;
+	    this.lifeMax = 50;
+	    this.width = 10;
+	    this.height = 10;
+	    this.colorMode = "rgb";
+	    this.color = {
+	      r: 255,
+	      g: 0,
+	      b: 0
+	    };
+	    this.mass = 10;
+		
+			myDiv = document.createElement("div"); // create element
+	    myDiv.className = "particle";
+	    myDiv.id = "p-" + options.PS.idCount;
+	    myDiv.style.webkitTransform = 'translateX(-5000px) translateY(-5000px) translateZ(0) scaleX(1) scaleY(1)';
+
+	    this.el = document.body.appendChild(myDiv);
+
+	    options.PS.idCount += 1;
+	
+		}
+	
+		Particle.prototype.step = function (world) {
+      
+      this.acceleration.add(world.gravity);
+      this.acceleration.add(world.wind);
+      this.velocity.add(this.acceleration);
+      this.location.add(this.velocity);
+    
+      this.life += 1;
+      if (this.life > this.lifeMax) {
+        this.dead = true;
+      }
+    };
+
+		Particle.prototype.draw = function () {
+
+	      var x = this.location.x - this.width/2,
+	        y = this.location.y - this.height/2,
+	        s = this.scale,
+	        o = this.opacity,
+	        w = this.width,
+	        h = this.height,
+	        cm = this.colorMode,
+	        c = this.color, 
+	        style = this.el.style;
+
+	       style.webkitTransform = 'translateX(' + x + 'px) translateY(' + y + 'px) translateZ(0) scaleX(' + s + ') scaleY(' + s + ')';
+	       style.MozTransform = 'translateX(' + x + 'px) translateY(' + y + 'px) translateZ(0) scaleX(' + s + ') scaleY(' + s + ')'; 
+	       style.OTransform = 'translateX(' + x + 'px) translateY(' + y + 'px) translateZ(0) scaleX(' + s + ') scaleY(' + s + ')';       
+	       style.opacity = o;
+	       style.width = w + "px";
+	       style.height = h + "px";
+	       style.background = cm + "(" + c.r + ", " + c.g + ", " + c.b + ")";
+
+	    };
+
+		Particle.prototype.destroy = function (particles) {
+	     var i;
+	     if (this.el) {
+	       document.body.removeChild(this.el);
+	     }
+	     for (i = particles.length - 1; i >= 0; i -= 1) {
+	       if (particles[i].el.id === this.el.id) {
+	         particles.splice(i, 1);
+	         break;
+	       }
+	     }
+
+	   };
+	   Particle.prototype.applyForce = function (force) { // F = M * A
+
+	      var f = force.clone();
+
+	      f.div(this.mass);
+	      this.acceleration.add(f);
+	   };
+		
+		return Particle;
+	
+	}());
+	
+	exports.Particle = Particle;
+	
+}(window));
+
+
+
 function setup (App) {
   
 	function getRandomNumber (low, high, flt) {
